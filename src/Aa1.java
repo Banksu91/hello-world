89f27a6a71084bef347f19c53c08626f3cf12d16
banksu91 <sk62176217@gmail.com>
2021-08-05 18:39:10 +0900

    initial

---
+++ src/Aa1.java
@@ -0,0 +1,8 @@
+
+public class Aa1 {
+
+	public static void main(String[] args) {
+		System.out.println("aa112");
+	}
+
+}
---
+++ src/asc/Ada.java
@@ -0,0 +1,9 @@
+package asc;
+
+public class Ada {
+
+	public static void main(String[] args) {
+
+	}
+
+}
